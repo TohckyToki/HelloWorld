89e46f1de156c03d17692e67d51bbc9a56e7a92a
TohckyToki <Tohcky@outlook.com>
2016-08-20 00:51:35 +0800

    上传文件(远程)

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+    formidable = require('formidable'),
+    fs = require('fs');
 
 var app = express();
 
@@ -8,7 +10,14 @@ var fortune = require('./lib/fortune');
 //Set view engine with handlebars
 var handlebars = require('express3-handlebars')
     .create({
-        defaultLayout: 'main'
+        defaultLayout: 'main',
+        helpers: {
+            section: function(name, options) {
+                if (!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
     });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -18,6 +27,8 @@ app.set('port', process.env.PROT || 3000);
 app.disable('x-powered-by');
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(require('body-parser')());
 
 app.use(function(req, res, next) {
     if (!res.locals.partials) res.locals.partials = {};
@@ -35,6 +46,31 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
+app.get('/mapuse', function(req, res) {
+    res.render('mapuse');
+})
+
+app.get('/newsletter', function(req, res) {
+    // we'll learn CSRF later,now just provide a virtual value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.get('/jquerytest', function(req, res) {
+    res.render('jquerytest');
+})
+
+app.get('/nursery-rhyme', function(req, res) {
+    res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res) {
+    res.json({
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck',
+    });
+});
 
 app.get('/headers', function(req, res) {
     res.set('Content-Type', 'text/plain');
@@ -50,6 +86,10 @@ app.get('/about', function(req, res) {
     });
 });
 
+app.get('/fileupload', function(req, res) {
+    res.render('fileupload');
+});
+
 app.get('/tours/hood-river', function(req, res) {
     res.render('tours/hood-river');
 });
@@ -62,6 +102,42 @@ app.get('/tours/request-group-rate', function(req, res) {
     res.render('tours/request-group-rate', {
         referrer: req.headers.referer
     });
+});
+
+app.post('/process', function(req, res) {
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        // if error happened，send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if error happened，redirect to error page.
+        console.log('Form (from querystring): ' + req.query.form);
+        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+        console.log('Name (from visible form field): ' + req.body.name);
+        console.log('Email (from visible form field): ' + req.body.email);
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.post('/fileupload', function(req, res) {
+    //创建表单上传
+    var form = new formidable.IncomingForm();
+    //设置编辑
+    form.encoding = 'utf-8';
+    //设置文件存储路径
+    form.uploadDir = "upload/";
+    //保留后缀
+    form.keepExtensions = true;
+    //设置单文件大小限制    
+    form.maxFieldsSize = 200 * 1024 * 1024;
+    //form.maxFields = 1000;  设置所以文件的大小总和
+    form.parse(req, function(err,fields,files) {
+        if(err){
+            console.log(err.info);
+        }
+        var types = files.upload.name.split('.'); //将文件名以.分隔，取得数组最后一项作为文件后缀名。
+        fs.renameSync(files.upload.path, "upload/" + String(types[0]) +"." + String(types[types.length-1]));
+    });
+    res.render('fileupload');
 });
 
 // Customise 404 page
