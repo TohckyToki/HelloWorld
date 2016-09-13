a921de44bdefb744c79597f4c4212c14ce849e0a
TohckyToki <Tohcky@outlook.com>
2016-09-13 21:19:20 +0800

    合并冲突

--- main.js
+++ main.js
@@ -1,183 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var formidable = require('formidable');
-var jqupload = require('jquery-file-upload-middleware');
-
-var app = express();
-
-//Reference the module which defined by myself
-var fortune = require('./lib/fortune');
-
-//Set view engine with handlebars
-var handlebars = require('express3-handlebars')
-    .create({
-        defaultLayout: 'main',
-        helpers: {
-            section: function(name, options) {
-                if (!this._sections) this._sections = {};
-                this._sections[name] = options.fn(this);
-                return null;
-            }
-        }
-    });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-app.set('port', process.env.PROT || 3000);
-
-app.disable('x-powered-by');
-
-app.use(express.static(__dirname + '/public'));
-
-app.use(require('body-parser')());
-
-app.use(function(req, res, next) {
-    if (!res.locals.partials) res.locals.partials = {};
-    res.locals.partials.weather = getWeatherData();
-    next();
-});
-
-app.use(function(req, res, next) {
-    res.locals.showTests = app.get('env') !== 'production' &&
-        req.query.test === '1';
-    next();
-});
-
-app.get('/', function(req, res) {
-    res.render('home');
-});
-
-app.get('/contest/vacation-photo', function(req, res) {
-    var now = new Date();
-    res.render('contest/vacation-photo', {
-        year: now.getFullYear(),
-        month: now.getMonth()
-    });
-});
-
-app.get('/mapuse', function(req, res) {
-    res.render('mapuse');
-})
-
-app.get('/newsletter', function(req, res) {
-    // we'll learn CSRF later,now just provide a virtual value
-    res.render('newsletter', { csrf: 'CSRF token goes here' });
-});
-
-app.get('/jquerytest', function(req, res) {
-    res.render('jquerytest');
-})
-
-app.get('/nursery-rhyme', function(req, res) {
-    res.render('nursery-rhyme');
-});
-
-app.get('/data/nursery-rhyme', function(req, res) {
-    res.json({
-        animal: 'squirrel',
-        bodyPart: 'tail',
-        adjective: 'bushy',
-        noun: 'heck',
-    });
-});
-
-app.get('/headers', function(req, res) {
-    res.set('Content-Type', 'text/plain');
-    var s = '';
-    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
-    res.send(s);
-});
-
-app.get('/about', function(req, res) {
-    res.render('about', {
-        fortune: fortune.getFortune(),
-        pageTestScript: '/qa/tests-about.js'
-    });
-});
-
-app.get('/tours/hood-river', function(req, res) {
-    res.render('tours/hood-river');
-});
-
-app.get('/tours/oregon-coast', function(req, res) {
-    res.render('tours/oregon-coast');
-});
-
-app.get('/tours/request-group-rate', function(req, res) {
-    res.render('tours/request-group-rate', {
-        referrer: req.headers.referer
-    });
-});
-
-app.post('/process', function(req, res) {
-    if (req.xhr || req.accepts('json,html') === 'json') {
-        // if error happened，send { error: 'error description' }
-        res.send({ success: true });
-    } else {
-        // if error happened，redirect to error page.
-        console.log('Form (from querystring): ' + req.query.form);
-        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-        console.log('Name (from visible form field): ' + req.body.name);
-        console.log('Email (from visible form field): ' + req.body.email);
-        res.redirect(303, '/thank-you');
-    }
-});
-
-app.post('/contest/vacation-photo/:year/:month', function(req, res) {
-    var form = new formidable.IncomingForm()
-    form.uploadDir = "upload/";
-    form.keepExtensions = true;
-    form.parse(req, function(err, fields, files) {
-        if (err) return res.redirect(303, '/error');
-        console.log('received fields:');
-        console.log(fields);
-        console.log('received files:');
-        console.log(files);
-        res.redirect(303, '/thank-you');
-    });
-});
-
-// Customise 404 page
-app.use(function(req, res) {
-    res.status(404);
-    res.render('404');
-});
-
-//Customise 500 page
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
-
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' +
-        app.get('port') + ';\npress Ctrl - C to terminate.');
-});
-
-function getWeatherData() {
-    return {
-        location: [{
-            name: 'Portland',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
-            weather: 'Overcast',
-            temp: '54.1 F (12.3 C)',
-        }, {
-            name: 'Bend',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
-            weather: 'Partly Cloudy',
-            temp: '55.0 F (12.8 C)',
-        }, {
-            name: 'Manzanita',
-            forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
-            iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
-            weather: 'Light Rain',
-            temp: '55.0 F (12.8 C)',
-        }, ],
-    };
-=======
 var express = require('express'),
     formidable = require('formidable'),
     fs = require('fs');
@@ -310,12 +130,12 @@ app.post('/fileupload', function(req, res) {
     //设置单文件大小限制    
     form.maxFieldsSize = 200 * 1024 * 1024;
     //form.maxFields = 1000;  设置所以文件的大小总和
-    form.parse(req, function(err,fields,files) {
-        if(err){
+    form.parse(req, function(err, fields, files) {
+        if (err) {
             console.log(err.info);
         }
         var types = files.upload.name.split('.'); //将文件名以.分隔，取得数组最后一项作为文件后缀名。
-        fs.renameSync(files.upload.path, "upload/" + String(types[0]) +"." + String(types[types.length-1]));
+        fs.renameSync(files.upload.path, "upload/" + String(types[0]) + "." + String(types[types.length - 1]));
     });
     res.render('fileupload');
 });
@@ -360,5 +180,4 @@ function getWeatherData() {
             temp: '55.0 F (12.8 C)',
         }, ],
     };
->>>>>>> remote
 }
